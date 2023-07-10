bef22a4808ecf983beae12d4a349e90eedea1357
nanashi <kevinandgunawan@gmail.com>
2023-07-10 16:09:52 +0900

    edited app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-print("     /|")
-print("    / |")
-print("   /  |")
-print("  /___|")
+character_name = "John"
+character_age = "35"
+
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + " years old")
